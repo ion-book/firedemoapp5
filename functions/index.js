06bc407109ad231e578eb4f071218d1948d03f13
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2017-08-19 17:34:23 -0500

    bienvenida a usuario especifico

--- functions/index.js
+++ functions/index.js
@@ -25,3 +25,32 @@ const APP_NAME = 'Firedemoapp5';
     
       return sendWelcomeEmail(email, displayName);
     });
+
+/**
+ * Enviar correo de despedida
+ */
+
+exports.sendByeEmail = functions.auth.user().onDelete(event => {
+    
+      const user = event.data;
+    
+      const email = user.email;
+      const displayName = user.displayName;
+    
+      return sendGoodbyEmail(email, displayName);
+    });
+
+// Enviar un mensaje de bienvenida al usuario especifico.
+function sendWelcomeEmail(email, displayName) {
+    const mailOptions = {
+      from: `${APP_NAME} <noreply@firebase.com>`,
+      to: email
+    };
+  
+    // The user subscribed to the newsletter.
+    mailOptions.subject = `Bienvenido a ${APP_NAME}!`;
+    mailOptions.text = `Hola ${displayName || ''}! Bienvenido a ${APP_NAME}. I hope you will enjoy our service.`;
+    return mailTransport.sendMail(mailOptions).then(() => {
+      console.log('Correo nuevo enviado:', email);
+    });
+  }
